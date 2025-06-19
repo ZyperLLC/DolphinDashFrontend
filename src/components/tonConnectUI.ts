026cdc763991409addb64442ab9115cd77a0b926
Rajatgupta94114 <guptarajat94114@gmail.com>
2025-06-19 21:32:01 +0530

    chore : update manifest file

--- src/components/tonConnectUI.ts
+++ src/components/tonConnectUI.ts
@@ -2,6 +2,6 @@
 import { TonConnectUI } from '@tonconnect/ui';
 
 export const tonConnectUI = new TonConnectUI({
-  manifestUrl: 'https://<your-domain>/tonconnect-manifest.json', // Replace with your manifest URL
+  manifestUrl: 'https://gateway.pinata.cloud/ipfs/bafkreiam4tqqodcvzmm6x73mfyoe2sveji3nk5d6vkixkt7gvd35xd7iai', // Replace with your manifest URL
   buttonRootId: 'ton-connect-button', // DOM element where button is rendered
 });
