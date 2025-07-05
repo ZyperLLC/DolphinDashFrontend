f8f33704212c6a47f12dc942702cf630016ae5bd
forrune <esoifeoma1@gmail.com>
2025-07-05 05:56:51 +0100

    Update Ton Connnect Manifest

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./App.tsx";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { UserProvider } from "./context/UserContext";
 
-// Use local dev manifest served from public/
-const manifestUrl = "http://localhost:5173/tonconnect-manifest.json";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <TonConnectUIProvider manifestUrl='https://jade-imperial-hawk-534.mypinata.cloud/ipfs/bafybeigkixu527m6rtupe3yimytsedkhroqve4bucaru6rrvkzw3ptcle4/'>
     <UserProvider>
-      <TonConnectUIProvider manifestUrl={manifestUrl}>
-        <App />
+        <StrictMode>
+          <App />
+        </StrictMode>
         <ToastContainer position="top-center" />
-      </TonConnectUIProvider>
     </UserProvider>
-  </StrictMode>
+  </TonConnectUIProvider>,
+
+
 );
